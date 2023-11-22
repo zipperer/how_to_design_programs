37af6aea7d18a0ffc03fafef2f81ddfa02456edd
zipperer <zipperer.andrew@gmail.com>
2023-11-21 20:35:42 -0600

    Exercise 2: for python version, add other ways to make string to document different ways that came to mind

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -7,3 +7,8 @@ class TestConcatenate(unittest.TestCase):
 
     def test_concatenate(self):
         self.assertEqual(prefix + '_' + suffix, "hello_world")
+        self.assertEqual(f'{prefix}_{suffix}', "hello_world")
+        self.assertEqual('{prefix}_{suffix}'.format(prefix=prefix, suffix=suffix), "hello_world")
+        self.assertEqual('{0}_{1}'.format(prefix, suffix), "hello_world")
+        self.assertEqual('{}_{}'.format(prefix, suffix), "hello_world")
+        self.assertEqual('_'.join([prefix, suffix]), "hello_world")
