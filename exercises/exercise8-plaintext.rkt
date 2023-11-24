c756644a735b302811327f702df0ba1b44489170
zipperer <zipperer.andrew@gmail.com>
2023-11-23 22:40:37 -0600

    Exercise 8: exercise8.rkt looks like gobbledygook. `$ racket exercise8.rkt` works as does `$ gracket exercise8.rkt`. exercise8-plaintext.rkt is exercise8.rkt without an image in the source code.

---
+++ exercises/exercise8-plaintext.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise8-plaintext) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/image)
+
+(define (image-is-tall-or-wide image)
+  (if (>= (image-height image) (image-width image))
+      "tall"
+      "wide"))
+
+(define (image-is-tall-wide-or-square image)
+  (if (> (image-height image) (image-width image))
+      "tall"
+      (if (= (image-height image) (image-width image))
+          "square"
+          "wide")))
+
+(image-is-tall-or-wide cat)
+"should be"
+"tall"
+
+(image-is-tall-wide-or-square (square 2 "solid" "black"))
+"should be"
+"square"
+
+(image-is-tall-wide-or-square (rectangle 2  1 "solid" "black"))
+"should be"
+"wide"
