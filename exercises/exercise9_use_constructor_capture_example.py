2e09a5b288b29c15dbfc3a3c868e8bf5a06ec498
zipperer <zipperer.andrew@gmail.com>
2023-11-26 07:59:28 -0600

    Exercise 9: provide example of case on type using constructors

---
+++ exercises/exercise9_use_constructor_capture_example.py
@@ -0,0 +1,25 @@
+import unittest
+
+def case_on_type_use_constructor(input_value):
+    match input_value:
+        case str(input_value_string):
+            print(input_value_string)
+            print(input_value_string + '!')
+        case int(input_value_int):
+            print(input_value_int)
+            print(input_value_int + 1)
+        case list(input_value_list):
+            print(input_value_list)
+            input_value_list.append('a')
+            print(input_value_list)
+        case _:
+            print('did not match constructor')
+    return type(input_value)
+
+class TestCaseOnTypeUseConstructor(unittest.TestCase):
+
+    def test_case_on_type_use_constructor(self):
+        self.assertEqual(case_on_type_use_constructor("hello"), str)
+        self.assertEqual(case_on_type_use_constructor(1), int)
+        self.assertEqual(case_on_type_use_constructor([1, 2, 3]), list)
+        self.assertEqual(case_on_type_use_constructor(set()), set)
