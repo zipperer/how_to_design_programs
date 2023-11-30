d69756d6b30fe317e6e580d06311979e2c5f202a
zipperer <zipperer.andrew@gmail.com>
2023-11-29 21:03:45 -0600

    Exercise 16: put images into folder, load images from folder, use context manager for images they close before test ends

--- exercises/exercise16.py
+++ exercises/exercise16.py
@@ -15,5 +15,7 @@ class TestImageArea(unittest.TestCase):
 
     def test_image_area(self):
         # when I saved these images, the resulting image had an extra pixel of width and height, so 
-        self.assertEqual(image_area(Image.open('racket_circle_radius_5_solid_red.png')), 121) # this is 11 * 11 rather than 10 * 10
-        self.assertEqual(image_area(Image.open('racket_square_length_5_solid_blue.png')), 36) # this is 6 * 6, rather than 5 * 5
+        with Image.open('images/racket_circle_radius_5_solid_red.png') as circle:
+            self.assertEqual(image_area(circle), 121) # this is 11 * 11 rather than 10 * 10
+        with Image.open('images/racket_square_length_5_solid_blue.png') as square:
+            self.assertEqual(image_area(square), 36) # this is 6 * 6, rather than 5 * 5
