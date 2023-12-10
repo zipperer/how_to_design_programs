01822f6eaf7da4099d0d79925b2a8633c971b590
zipperer <zipperer.andrew@gmail.com>
2023-12-09 20:08:32 -0600

    Exercise 32: .rkt only: not a program

---
+++ exercises/exercise32.rkt
@@ -0,0 +1,21 @@
+; Exercise 32. Most people no longer use desktop computers just to run 
+; applications but also employ cell phones, tablets, and their cars’ 
+; information control screen. Soon people will use wearable computers
+; in the form of intelligent glasses, clothes, and sports gear. In the 
+; somewhat more distant future, people may come with built-in bio 
+; computers that directly interact with body functions. Think of ten 
+; different forms of events that software applications on such computers 
+; will have to deal with.
+
+; 1. Wake -- restart day processes
+; 2. Sleep -- suspend day processes
+; 3. Digestion begin
+; 4. Heartbeat
+; 5. Breath inhale
+; 6. Breath exhale
+; 7. Blood pressure change
+; 8. New object in field of view
+; 9. Opponent nears
+;10. Perspire begin
+;
+
