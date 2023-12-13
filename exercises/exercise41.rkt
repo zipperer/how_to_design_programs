56940dfccdf06ad83e9fee6189fd8d18be9df142
zipperer <zipperer.andrew@gmail.com>
2023-12-13 17:50:58 -0600

    Exercise 41: give name to procedure to add tree to background. use procedure

--- exercises/exercise41.rkt
+++ exercises/exercise41.rkt
@@ -14,14 +14,16 @@
   (underlay/xy (circle (* 1/2 CAR-HEIGHT) "solid" "green")
                (- (* 1/3 CAR-HEIGHT) 1) (* 1 CAR-HEIGHT)
                (rectangle (* 1/5 CAR-HEIGHT) (* 2 CAR-HEIGHT) "solid" "brown")))
-(define BACKGROUND-WITH-TREE-1 (place-image tree (image-width CAR) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND))
-(define BACKGROUND-WITH-TREE-2 (place-image tree (* 3/2 (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND-WITH-TREE-1))
-(define BACKGROUND-WITH-TREE-3 (place-image tree (* 5/2 (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND-WITH-TREE-2))
-(define BACKGROUND-WITH-TREE-4 (place-image tree (* 9/2 (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND-WITH-TREE-3))
-(define BACKGROUND-WITH-TREE-5 (place-image tree (* 12/2 (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND-WITH-TREE-4))
-(define BACKGROUND-WITH-TREE-6 (place-image tree (* 15/2 (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND-WITH-TREE-5))
-(define BACKGROUND-WITH-TREE-7 (place-image tree (* 19/2 (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) BACKGROUND-WITH-TREE-6))
-BACKGROUND-WITH-TREE-7
+(define (add-tree-to-background-at-position-of-car-lengths background car-lengths)
+  (place-image tree (* car-lengths (image-width CAR)) (- BACKGROUND-HEIGHT (* 1/4 (image-height tree))) background))
+(define BACKGROUND-WITH-TREE-1 (add-tree-to-background-at-position-of-car-lengths BACKGROUND 1))
+(define BACKGROUND-WITH-TREE-2 (add-tree-to-background-at-position-of-car-lengths BACKGROUND-WITH-TREE-1 3/2))
+(define BACKGROUND-WITH-TREE-3 (add-tree-to-background-at-position-of-car-lengths BACKGROUND-WITH-TREE-2 5/2))
+(define BACKGROUND-WITH-TREE-4 (add-tree-to-background-at-position-of-car-lengths BACKGROUND-WITH-TREE-3 9/2))
+(define BACKGROUND-WITH-TREE-5 (add-tree-to-background-at-position-of-car-lengths BACKGROUND-WITH-TREE-4 12/2))
+(define BACKGROUND-WITH-TREE-6 (add-tree-to-background-at-position-of-car-lengths BACKGROUND-WITH-TREE-5 15/2))
+(define BACKGROUND-WITH-TREE-7 (add-tree-to-background-at-position-of-car-lengths BACKGROUND-WITH-TREE-6 19/2))
+
 (define Y-CAR (- BACKGROUND-HEIGHT (* 1/2 CAR-HEIGHT)))
 
 ; WorldState -> Image
