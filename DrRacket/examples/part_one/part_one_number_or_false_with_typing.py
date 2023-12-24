59290e9f6a8c384ef8ddd284ee31df344c37b176
zipperer <zipperer.andrew@gmail.com>
2023-12-24 11:47:37 -0600

    typing and mypy: first example: new version of part_one_number_or_false.py that uses typing module. ran this (in container with python 3.12 and mypy) with $ python3 -m mypy part_one_number_or_false_with_typing.py

---
+++ DrRacket/examples/part_one/part_one_number_or_false_with_typing.py
@@ -0,0 +1,36 @@
+from typing import Union, Literal
+Number = Union[int, float, complex] # alternatively int | float | complex
+NumberOrFalse = Union[Number, Literal[False]]
+
+# NumberOrFalse -> Number
+# adds 3 to the given number; 3 otherwise
+
+def add3 (number_or_false : NumberOrFalse) -> Number:
+    match number_or_false:
+        case False:
+            return 3
+        case int(number) | float(number) : # does not handle complex numbers or other number types from libraries
+            return 3 + number
+        case complex(real=input_complex_number_real_part, imag=input_complex_number_imaginary_part):
+            return complex(real=(3 + input_complex_number_real_part), imag=input_complex_number_imaginary_part)
+
+def add3_without_using_match_keyword(number_or_false : NumberOrFalse) -> Number:
+    if number_or_false is False:
+        return 3
+    else:
+        number = number_or_false
+        return 3 + number
+
+import pytest
+
+def test_add3():
+    assert add3(False) == 3
+    assert add3(1) == 4
+    assert add3(1.0) == 4.0
+    assert add3(complex(real=0, imag=0)) == 3+0j
+
+def test_add3_without_using_match_keyword_without_using_match_keyword():
+    assert add3_without_using_match_keyword(False) == 3
+    assert add3_without_using_match_keyword(1) == 4
+    assert add3_without_using_match_keyword(1.0) == 4.0
+    assert add3_without_using_match_keyword(complex(real=0, imag=0)) == 3+0j
