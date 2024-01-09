b47dc4110a72d539f890a269f2cf9b67152801eb
zipperer <zipperer.andrew@gmail.com>
2024-01-09 12:57:20 -0600

    Exercise 113 helpers:
    posn.py:
    add function is_posn that is a predicate
    add function make_posn that is a function to create an instance of Posn

--- exercises/posn.py
+++ exercises/posn.py
@@ -1,8 +1,20 @@
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Any
 Number = Union[int,float]
+
+from local_utilities import is_number
 
 @dataclass
 class Posn:
     x: Number
     y: Number
+
+def is_posn(value : Any) -> bool:
+    return (isinstance(value, Posn) and
+            is_number(value.x) and
+            is_number(value.y))
+
+def make_posn(x : Number,
+              y : Number
+              ) -> Posn:
+    return Posn(x=x, y=y)
