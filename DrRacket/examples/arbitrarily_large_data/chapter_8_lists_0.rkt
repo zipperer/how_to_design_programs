5ac668242b20005ff06da38ea81f2c742be7c97a
zipperer <zipperer.andrew@gmail.com>
2024-01-12 21:33:27 -0600

    Exercise 138

--- DrRacket/examples/arbitrarily_large_data/chapter_8_lists_0.rkt
+++ DrRacket/examples/arbitrarily_large_data/chapter_8_lists_0.rkt
@@ -312,4 +312,35 @@
 ; Both templates:
 ; - have a cond that checks (a) empty? and (b) cons?.
 ; - in the cond clause for cons?, have selector expressions for first and rest
-; - in the cond clause for cons?, have natural recursion expression 
+; - in the cond clause for cons?, have natural recursion expression
+
+; Exercise 138
+; Here is a data definition for representing sequences of amounts of money:
+; A List-of-amounts is one of: 
+; – '()
+; – (cons PositiveNumber List-of-amounts)
+
+; Create some examples to make sure you understand the data definition.
+; examples:
+; '() is a List-of-amounts
+; (cons 1 '()) is a List-of-amounts
+; (cons 2 (cons 1 '())) is a List-of-amounts
+; (cons 10 (cons 9 (cons 8 (cons 4 '())))) is a List-of-amountss
+
+; Also add an arrow for the self-reference.
+; - Arrow from List-of-amounts in second clause of data definition to name of data definition
+
+; Design the sum function, which consumes a List-of-amounts and computes the sum of the amounts.
+; Use DrRacket’s stepper to see how (sum l) works for a short list l in List-of-amounts.
+
+; List-of-amounts -> Number
+(check-expect (sum '()) 0)
+(check-expect (sum (cons 1 '())) 1)
+(check-expect (sum (cons 2 (cons 1 '()))) 3)
+(check-expect (sum (cons 10 (cons 9 (cons 8 (cons 4 '()))))) 31)
+(define (sum list-of-amounts)
+  (cond
+    [(empty? list-of-amounts) 0]
+    [(cons? list-of-amounts)
+     (+ (first list-of-amounts)
+     (sum (rest list-of-amounts)))]))
