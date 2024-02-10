3f1fd4d92d8ba1523e1c849d95f299b98dbf1494
zipperer <zipperer.andrew@gmail.com>
2024-02-09 19:01:36 -0600

    exercise 161: .py: after change to 14$ per hour and factoring out WAGE_DOLLARS_PER_HOUR

--- exercises/exercise161.py
+++ exercises/exercise161.py
@@ -1,8 +1,10 @@
 from typing import List
 
+WAGE_DOLLARS_PER_HOUR = 14
+
 def wage (h : int) -> int:
     '''computes the wage for h hours of work'''
-    return 14 * h
+    return WAGE_DOLLARS_PER_HOUR * h
 
 # wage_star b/c racket version uses wage*
 def wage_star (number_of_hours_each_employee_worked : List[int]) -> List[int]:
@@ -21,7 +23,7 @@ def wage_star (number_of_hours_each_employee_worked : List[int]) -> List[int]:
 
 def test_wage_star():
     assert wage_star([]) == []
-    assert wage_star([1, 2, 3]) == [14, 28, 42]
-    assert wage_star([4, 3, 5]) == [56, 42, 70]
-    assert wage_star([28]) == [392]
-    assert wage_star([4, 2]) == [56, 28]
+    assert wage_star([1, 2, 3]) == [wage(1), wage(2), wage(3)]
+    assert wage_star([4, 3, 5]) == [wage(4), wage(3), wage(5)]
+    assert wage_star([28]) == [wage(28)]
+    assert wage_star([4, 2]) == [wage(4), wage(2)]
